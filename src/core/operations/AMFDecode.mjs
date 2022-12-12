f41b9c2b1f74a2d4018a2a2b7fc6816939d122cd
Raka-loah <i@lotc.cc>
2022-12-12 15:47:22 +0800

    v9.55.0

--- src/core/operations/AMFDecode.mjs
+++ src/core/operations/AMFDecode.mjs
@@ -2,6 +2,8 @@
  * @author n1474335 [n1474335@gmail.com]
  * @copyright Crown Copyright 2022
  * @license Apache-2.0
+ *
+ * Modified by Raka-loah@github for zh-CN i18n
  */
 
 import Operation from "../Operation.mjs";
@@ -19,15 +21,15 @@ class AMFDecode extends Operation {
     constructor() {
         super();
 
-        this.name = "AMF Decode";
+        this.name = "AMF解码";
         this.module = "Encodings";
-        this.description = "Action Message Format (AMF) is a binary format used to serialize object graphs such as ActionScript objects and XML, or send messages between an Adobe Flash client and a remote service, usually a Flash Media Server or third party alternatives.";
+        this.description = "Action Message Format (AMF) 是一种用于序列化对象图（例如ActionScript对象和XML）的二进制格式，通常用于Adobe Flash客户端与服务器（Flash Media Server或其它第三方替代品）的消息传递。";
         this.infoURL = "https://wikipedia.org/wiki/Action_Message_Format";
         this.inputType = "ArrayBuffer";
         this.outputType = "JSON";
         this.args = [
             {
-                name: "Format",
+                name: "格式",
                 type: "option",
                 value: ["AMF0", "AMF3"],
                 defaultIndex: 1
